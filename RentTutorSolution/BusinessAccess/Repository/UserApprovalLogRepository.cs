a4c036b6cc2c0f9e76078b6eb24bddf6834416b3
VanHa1006 <106126039+VanHa1006@users.noreply.github.com>
2024-08-09 02:10:44 +0700

    Done Request Tutor Register

---
+++ RentTutorSolution/BusinessAccess/Repository/UserApprovalLogRepository.cs
@@ -0,0 +1,15 @@
+﻿using BusinessAccess.DAO;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccess.Repository
+{
+    public class UserApprovalLogRepository : GenericRepository<UserApprovalLog>
+    {
+        public UserApprovalLogRepository() { }
+    }
+}
