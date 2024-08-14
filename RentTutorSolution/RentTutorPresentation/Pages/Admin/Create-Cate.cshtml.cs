38bf6c833d20b290ed953aae878c515fb15d87ee
Thinhnc1904 <cuongthinh5213@gmail.com>
2024-08-15 01:18:21 +0700

    Update Homepage, Creatr Category

---
+++ RentTutorSolution/RentTutorPresentation/Pages/Admin/Create-Cate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RentTutorPresentation.Pages.Admin
+{
+    public class Create_CateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
